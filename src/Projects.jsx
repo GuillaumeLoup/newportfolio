7c6665276b485bcd23cdda98f29bad23cf359157
Guillaume Loup <guillaume.loup@live.fr>
2020-01-21 09:02:04 +0100

    master

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -26,9 +26,8 @@ class Projects extends Component {
              le menu du jour ou la carte quand il le souhaite. PS: ajouter /admin
               à l'url pour voir la partie administrateur concernant le menu du jour
                et ajouter /admin2 à l'url pour voir la partie administrateur
-                concernant la carte du restaurant.
-                Technos utilisées: ReactJs, NodeJs, Express
-                 Ce site est consultable via ce lien: </p>
+                concernant la carte du restaurant. 
+                <span> Technos utilisées: ReactJs, NodeJs, Express </span></p>
                  <a href="https://vigorous-snyder-06c6f0.netlify.com/" rel="noopener noreferrer" target="_blank"> lien vers le site</a>
                 <img className="close-guillemets" src={!backgroundColor ? close : closenoir} alt="close" />
                  </div>
@@ -39,9 +38,8 @@ class Projects extends Component {
           <img className="open-guillemets2" src={!backgroundColor ? open: opennoir} alt="open" />
           <p>Il m'a été demandé de refaire le site de la société SQAL
            en respectant une maquette réalisée par l'UX designer de notre
-            société. Bien sûr le site est responsive.
-            Technos utilisées: ReactJs, Styled Components
-             Ce site est consultable via ce lien:</p>
+            société. Bien sûr le site est responsive. 
+            <span> Technos utilisées: ReactJs, Styled Components </span></p>
              <a href="https://www.sqal.io/" rel="noopener noreferrer" target="_blank"> lien vers le site</a>
              <img className="close-guillemets2" src={!backgroundColor ? close : closenoir} alt="close" />
                  </div>
@@ -58,9 +56,8 @@ class Projects extends Component {
            de choisir un avocat selon ses compétences et de pouvoir
             le contacter via une vidéconférence. Ceci est faisaible
              dans la rubrique "Nos conseillers" il faut clqiuer sur
-              "choisissez votre conseiller" et "contacter ce conseiller".
-              Technos utilisées: ReactJs, Styled Components
-               Ce site est consultable via ce lien: </p>
+              "choisissez votre conseiller" et "contacter ce conseiller". 
+              <span> Technos utilisées: ReactJs, Styled Components </span></p>
                  <a href="https://dazzling-kowalevski-e41b9f.netlify.com/" rel="noopener noreferrer" target="_blank"> lien vers le site</a>
                  <img className="close-guillemets" src={!backgroundColor ? close : closenoir} alt="close" />
                  </div>
@@ -73,9 +70,8 @@ class Projects extends Component {
            avec en bonus un easter-egg caché dans celui-ci. J'ai pris le parti
             de concevoir un site internet regroupant des cartes à collectionner
              à retrouver dans les chocolats de nos partenaires (Kinder, Milka, etc...).
-              spoiler: cliquez 21 fois sur l'oeuf vert situé en haut de page.
-              Technos utilisées: ReactJs
-               Ce site est consultable via ce lien:</p>
+              spoiler: cliquez 21 fois sur l'oeuf vert situé en haut de page. 
+              <span> Technos utilisées: ReactJs </span></p>
              <a href="https://dazzling-shannon-cd83bf.netlify.com/" rel="noopener noreferrer" target="_blank"> lien vers le site</a>
                  <img className="close-guillemets2" src={!backgroundColor ? close : closenoir} alt="close" />
                  </div>
@@ -88,7 +84,7 @@ class Projects extends Component {
           <div className="text-explain">
           <img className="open-guillemets" src={!backgroundColor ? open : opennoir} alt="open" />
           <p>J'ai développé une petite web-app permettant de voir le classement de ligue 1 et en cliquant sur une équipe
-           de voir les résultats de chaque match depuis le début de la saison et les matchs à venir. Technos utilisées: ReactJs</p>
+           de voir les résultats de chaque match depuis le début de la saison et les matchs à venir. <span> Technos utilisées: ReactJs </span></p>
                  <a href="https://ligue1conforama-snapp.netlify.com/" rel="noopener noreferrer" target="_blank"> lien vers le site</a>
                  <img className="close-guillemets" src={!backgroundColor ? close : closenoir} alt="close" />
           </div>
