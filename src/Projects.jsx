1a74902ae8d8f427090bb002c00a89c71a642600
Guillaume Loup <guillaume.loup@live.fr>
2020-01-21 08:55:23 +0100

    ajout ligue 1

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -27,6 +27,7 @@ class Projects extends Component {
               à l'url pour voir la partie administrateur concernant le menu du jour
                et ajouter /admin2 à l'url pour voir la partie administrateur
                 concernant la carte du restaurant.
+                Technos utilisées: ReactJs, NodeJs, Express
                  Ce site est consultable via ce lien: </p>
                  <a href="https://vigorous-snyder-06c6f0.netlify.com/" rel="noopener noreferrer" target="_blank"> lien vers le site</a>
                 <img className="close-guillemets" src={!backgroundColor ? close : closenoir} alt="close" />
@@ -39,8 +40,9 @@ class Projects extends Component {
           <p>Il m'a été demandé de refaire le site de la société SQAL
            en respectant une maquette réalisée par l'UX designer de notre
             société. Bien sûr le site est responsive.
+            Technos utilisées: ReactJs, Styled Components
              Ce site est consultable via ce lien:</p>
-             <a href="https://sqal-website.herokuapp.com/" rel="noopener noreferrer" target="_blank"> lien vers le site</a>
+             <a href="https://www.sqal.io/" rel="noopener noreferrer" target="_blank"> lien vers le site</a>
              <img className="close-guillemets2" src={!backgroundColor ? close : closenoir} alt="close" />
                  </div>
                  <div className="img2">
@@ -56,7 +58,8 @@ class Projects extends Component {
            de choisir un avocat selon ses compétences et de pouvoir
             le contacter via une vidéconférence. Ceci est faisaible
              dans la rubrique "Nos conseillers" il faut clqiuer sur
-              "choisissez votre conseiller" et "contacter ce conseiller"
+              "choisissez votre conseiller" et "contacter ce conseiller".
+              Technos utilisées: ReactJs, Styled Components
                Ce site est consultable via ce lien: </p>
                  <a href="https://dazzling-kowalevski-e41b9f.netlify.com/" rel="noopener noreferrer" target="_blank"> lien vers le site</a>
                  <img className="close-guillemets" src={!backgroundColor ? close : closenoir} alt="close" />
@@ -71,13 +74,25 @@ class Projects extends Component {
             de concevoir un site internet regroupant des cartes à collectionner
              à retrouver dans les chocolats de nos partenaires (Kinder, Milka, etc...).
               spoiler: cliquez 21 fois sur l'oeuf vert situé en haut de page.
+              Technos utilisées: ReactJs
                Ce site est consultable via ce lien:</p>
              <a href="https://dazzling-shannon-cd83bf.netlify.com/" rel="noopener noreferrer" target="_blank"> lien vers le site</a>
                  <img className="close-guillemets2" src={!backgroundColor ? close : closenoir} alt="close" />
                  </div>
                  <div className="img4">
           </div>
-                 </div>
+          </div>
+          <div className={backgroundColor ? "wrapper-project" : "wrapper-project-white" }>
+          <div className="img5">
+          </div>
+          <div className="text-explain">
+          <img className="open-guillemets" src={!backgroundColor ? open : opennoir} alt="open" />
+          <p>J'ai développé une petite web-app permettant de voir le classement de ligue 1 et en cliquant sur une équipe
+           de voir les résultats de chaque match depuis le début de la saison et les matchs à venir. Technos utilisées: ReactJs</p>
+                 <a href="https://ligue1conforama-snapp.netlify.com/" rel="noopener noreferrer" target="_blank"> lien vers le site</a>
+                 <img className="close-guillemets" src={!backgroundColor ? close : closenoir} alt="close" />
+          </div>
+          </div>
         
       </div>
     )
